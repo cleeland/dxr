255036b936410015bc0fa4898a4a7954a6d11baf
Chris Cleeland <ccleeland@exegy.net>
2012-09-05 09:08:11 -0500

    Custom changes to get this to work with Exegy build system.

--- xref-tools/cxx-clang/set-env.sh
+++ xref-tools/cxx-clang/set-env.sh
@@ -5,5 +5,9 @@ wrapperscript=$DXRSRC/xref-tools/cxx-clang/cc.pl
 export CC="$wrapperscript clang $SRCDIR"
 export CXX="$wrapperscript clang++ $SRCDIR"
 export CCC=$CXX
+
+export CC_clang=$CC
+export CCC_clang=$CCC
+
 export DXR_INDEX_OUTPUT="$OBJDIR"
 export DXR_ENV_SET="true"
